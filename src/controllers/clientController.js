cc5ae76c07c3ff52c5e7a278a14db6725f4ce6f3
dev-rafa1707 <rafa1707@gmail.com>
2023-05-07 09:13:02 -0300

    add def update cliente

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -39,14 +39,31 @@ const clientController = {
                 estado: reqClient.estado,
                 cep:  reqClient.cep,
             });
-            return res.status(200).json(client)
+            return res.status(200).json(client);
         } catch (error) {
-            return res.status(500).json({ error: error.message})
+            return res.status(500).json({ error: error.message});
         }
     },
 
     async update(req,res) {
-
+        try {
+            const {id} = req.params;
+            const {nome, email, cidade, estado, cep} = req.body;
+            const client = await Client.findByPk(id);
+            if (!client) {
+                return res.status(404).json({message: 'Client not found'});
+            }
+            const clientUpdated = await client.update({
+                nome,
+                email,
+                cidade,
+                estado,
+                cep
+            });
+            return res.status(200).json(clientUpdated);
+        } catch (error) {
+            return res.status(500).json({ message: error.message});
+        }
     },
 
     async delete(req,res) {
