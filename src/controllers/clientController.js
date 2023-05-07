6b9c41776717460583509e25ca87dea86291df50
dev-rafa1707 <rafa1707@gmail.com>
2023-05-07 09:40:23 -0300

    add def delete cliente

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -33,11 +33,11 @@ const clientController = {
         try {
             const {nome, email, cidade, estado, cep} = req.body;
             const client = await Client.create({
-                nome: reqClient.nome,
-                email: reqClient.email,
-                cidade: reqClient.cidade,
-                estado: reqClient.estado,
-                cep:  reqClient.cep,
+                nome,
+                email,
+                cidade,
+                estado,
+                cep,
             });
             return res.status(200).json(client);
         } catch (error) {
@@ -67,11 +67,17 @@ const clientController = {
     },
 
     async delete(req,res) {
-
+        try {
+            const {id} = req.params;
+            const client = await Client.findByPk(id);
+            if (!client) {
+                return res.status(404).json({ message: 'Client not found'})
+            } await client.destroy();
+            return res.json({message: 'Client deleted successfully'});
+        } catch (error) {
+            return res.status(500).json({message: error.message});
+        }
     },
-
-
-
 };
 
 module.exports = clientController
