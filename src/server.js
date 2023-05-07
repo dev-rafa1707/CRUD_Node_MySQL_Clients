9f78dc6dba0286c7f137772ed7ca3b0b05d033e3
dev-rafa1707 <rafa1707@gmail.com>
2023-05-07 10:57:26 -0300

    corrige routes e acesso ao banco de dados

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 const express = require('express')
-// const routes = require('./routes/routes')
+const routes = require('./routes/routes')
 
 const server = express()
 const PORT = process.env.PORT || 3000
 
 server.use(express.json())
 server.use(express.urlencoded({extended:true}))
-// server.use(routes)
+server.use(routes)
 
 
 
